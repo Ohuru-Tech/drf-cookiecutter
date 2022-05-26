9a42301330c14be94fa5706dce16b82db341f10b
amartya-dev <amarkaushik1999@gmail.com>
2022-05-26 17:22:52 +0530

    fix: separate json as variable to support cookiecutter

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/examples/user_requests.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/examples/user_requests.py
@@ -4,12 +4,14 @@ token = getattr(settings, "SAMPLE_AUTH_TOKEN")
 origin = getattr(settings, "HOSTED_DOMAIN")
 auth = f'-H "Authorization: Bearer {token}"' if token else ""
 
+profile_update_body = {"profile_pic": "some_pic.jpg"}
+
 profile_update_curl = f"""
 # Update the profile pic of a user
 curl \\
   -X POST \\
   -H "Content-Type: application/json" \\
   {auth} \\
-  -d '{{"profile_pic": "some_pic.jpg"}}' \\
+  -d '{profile_update_body}' \\
   "{origin}/v1/"
 """  # noqa
